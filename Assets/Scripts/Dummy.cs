1ee0b8442fc38a92fca3a03fb7efdbd5e99597b4
LisaBourdon <lisa.bourdon@epitech.eu>
2023-06-26 12:43:07 +0200

    Create Dummy with script to take damage

---
+++ Assets/Scripts/Dummy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    public float hp = 10;
+    public void getDamage(float damage)
+    {
+        hp -= damage;
+        // change color to red when hit and return to pink after 0.1 seconds
+        GetComponent<SpriteRenderer>().color = Color.red;
+        Invoke("ResetColor", 0.1f);
+        // destroy the object when hp is 0
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
