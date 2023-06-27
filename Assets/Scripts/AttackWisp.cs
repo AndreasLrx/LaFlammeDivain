5d22ce5e1d431690cf2fac53682bad971683dd3c
Andréas Leroux <andreas.leroux@epitech.eu>
2023-06-27 23:25:13 +0200

    Replace wisps at correct place in wisps group
    Close #37

--- Assets/Scripts/AttackWisp.cs
+++ Assets/Scripts/AttackWisp.cs
@@ -6,6 +6,16 @@ public class AttackWisp : Wisp
 {
     public float range = 5;
 
+    void Start()
+    {
+        color = new Color(
+            Random.Range(0.4f, 1f),
+            Random.Range(0.4f, 1f),
+            Random.Range(0.4f, 1f)
+        );
+        ResetColor();
+    }
+
     protected override IEnumerator OnActivate()
     {
         return SmoothMovement((Vector2)playerObject.transform.position + Player().AimedDirection() * range);
