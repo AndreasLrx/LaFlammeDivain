ba334a8c10ddd26851f630288d66cc539aaf245e
EnzoDrapron <enzo.drapron@gmail.com>
2023-07-18 17:12:28 +0200

    Add Main Menu
    Close #21

---
+++ Assets/Scripts/OptionMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionMenu : MonoBehaviour
+{
+    //For the option menu, we want to be able to change the volume of the game and control.
+
+    public void Audio()
+    {
+        Debug.Log("Audio");
+    }
+
+    public void Controls()
+    {
+        Debug.Log("Controls");
+    }
+}
